06b92b7742b60d4a56d530287d00f6fa238493ee
Roro <183158846+roronotalt@users.noreply.github.com>
2024-11-09 20:36:36 -0500

    github redirect url

--- src/lib/server/oauth.ts
+++ src/lib/server/oauth.ts
@@ -1,5 +1,5 @@
 import { GitHub } from "arctic";
-import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from "$env/static/private";
+import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, REDIRECT_URL } from "$env/static/private";
 
 // TODO: Update redirect URI
-export const github = new GitHub(GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, "http://localhost:5173/login/github/callback");
+export const github = new GitHub(GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, REDIRECT_URL);
